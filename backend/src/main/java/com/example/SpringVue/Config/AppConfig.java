91465b9d3afdbac2a0417f7d8de136a0c5392832
BurakAndarman <brkandrmn@hotmail.com>
2023-12-11 17:03:00 +0300

    Ehcache config done

--- backend/src/main/java/com/example/SpringVue/Config/AppConfig.java
+++ backend/src/main/java/com/example/SpringVue/Config/AppConfig.java
@@ -1,9 +1,21 @@
 package com.example.SpringVue.Config;
 
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.ExpiryPolicyBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.jsr107.Eh107Configuration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import javax.cache.Caching;
+import javax.cache.spi.CachingProvider;
+import javax.cache.CacheManager;
+import java.time.Duration;
+import java.util.List;
 
 @Configuration
 @EnableCaching
@@ -12,5 +24,24 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public CacheManager EhcacheManager() {
+
+        CacheConfiguration<String, List> cacheConfig = CacheConfigurationBuilder
+                .newCacheConfigurationBuilder(String.class,List.class, ResourcePoolsBuilder.newResourcePoolsBuilder()
+                        .offheap(10, MemoryUnit.MB)
+                        .build())
+                .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofHours(5)))
+                .build();
+
+        CachingProvider cachingProvider = Caching.getCachingProvider();
+        CacheManager cacheManager = cachingProvider.getCacheManager();
+
+        javax.cache.configuration.Configuration<String,List> configuration = Eh107Configuration.fromEhcacheCacheConfiguration(cacheConfig);
+        cacheManager.createCache("userNewsCache",configuration);
+
+        return cacheManager;
     }
 }
