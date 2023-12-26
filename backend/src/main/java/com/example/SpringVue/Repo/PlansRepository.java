3b4bf94f4f20be0245f6a814c533e699a56cb2c9
BurakAndarman <brkandrmn@hotmail.com>
2023-12-26 23:22:19 +0300

    - findAllByUser method created in PlansRepository

--- backend/src/main/java/com/example/SpringVue/Repo/PlansRepository.java
+++ backend/src/main/java/com/example/SpringVue/Repo/PlansRepository.java
@@ -1,7 +1,13 @@
 package com.example.SpringVue.Repo;
 
 import com.example.SpringVue.Entity.Plans;
+import com.example.SpringVue.Entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PlansRepository extends JpaRepository<Plans, Integer> {
+
+    List<Plans> findAllByUser(User user);
+
 }
